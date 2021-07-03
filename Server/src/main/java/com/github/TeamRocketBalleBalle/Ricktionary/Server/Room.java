a8f26e334997f739c779121c0af25fa11b0be769
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2021-07-03 21:15:41 +0530

    change logs a little bit

--- Server/src/main/java/com/github/TeamRocketBalleBalle/Ricktionary/Server/Room.java
+++ Server/src/main/java/com/github/TeamRocketBalleBalle/Ricktionary/Server/Room.java
@@ -2,14 +2,23 @@ package com.github.TeamRocketBalleBalle.Ricktionary.Server;
 
 // TODO: implement this
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Room implements Runnable {
+    private final Logger logger;
+    {
+        logger = LoggerFactory.getLogger("Ricktionary.Server.Room");
+    }
     public void add(Player player) {}
 
     public boolean isReady() {
         return true;
     }
 
-    public void run() {}
+    public void run() {
+        logger.info("New Room created");
+    }
 
     public void end() {}
 }
--- Server/src/main/java/com/github/TeamRocketBalleBalle/Ricktionary/Server/Server.java
+++ Server/src/main/java/com/github/TeamRocketBalleBalle/Ricktionary/Server/Server.java
@@ -65,7 +65,6 @@ public class Server {
                 if (currentRoom.isReady()) {
                     Thread thread = new Thread(currentRoom);
                     thread.start();
-                    logger.info("New thread created");
                     rooms.add(0, new Room());
                 }
             }
