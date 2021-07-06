7ebb333d0e11cbfa170de1982b9f01f470fdda37
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2021-07-06 22:42:02 +0530

    update dictionary & correct spellings

--- .github/actions/spelling/expect/README.md
+++ .github/actions/spelling/expect/README.md
@@ -1,10 +1,11 @@
 The contents of each `.txt` file in this directory are merged together.
 
-| File                     | Description                                                                               |
-| ------------------------ | ----------------------------------------------------------------------------------------- |
-| [expect](expect.txt)     | is the main list of expected items -- that are just some variables names you can't change |
-| [plantuml](plantuml.txt) | plantuml syntax keywords                                                                  |
-| [web](web.txt)           | is a sample for web/html related items                                                    |
+| File                              | Description                                                                               |
+| --------------------------------- | ----------------------------------------------------------------------------------------- |
+| [expect](expect.txt)              | is the main list of expected items -- that are just some variables names you can't change |
+| [plantuml](plantuml.txt)          | plantuml syntax keywords                                                                  |
+| [ui exceptions](uiexceptions.txt) | the words that have to be exempted because... its easy to change var names with an IDE    |
+| [web](web.txt)                    | is a sample for web/html related items                                                    |
 
 These terms are things which temporarily exist in the project, but which
 aren't necessarily words.
--- Server/src/main/java/com/github/TeamRocketBalleBalle/Ricktionary/Server/DummyClient.java
+++ Server/src/main/java/com/github/TeamRocketBalleBalle/Ricktionary/Server/DummyClient.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
 public class DummyClient {
     public void test() {
         try {
-            Logger logger = LoggerFactory.getLogger("Rictionary.dummyClient");
+            Logger logger = LoggerFactory.getLogger("Ricktionary.dummyClient");
             Scanner kb = new Scanner(System.in);
 
             Socket socket = new Socket("127.0.0.1", 5000);
@@ -23,93 +23,74 @@ public class DummyClient {
             ObjectOutputStream out2 = new ObjectOutputStream(socket.getOutputStream());
             logger.debug("starting loop...");
             while (true) {
-                //                if (0 < input.available()) {
+                // if (0 < input.available()) {
                 logger.debug("Data available to read");
                 Object o = input.readObject();
                 logger.debug("{}", o);
                 ServerPacket serverPacket = (ServerPacket) o;
-                logger.debug(
-                        "Received: {}, {}", serverPacket.getPacketType(), serverPacket.getOrder());
+                logger.debug("Received: {}, {}", serverPacket.getPacketType(), serverPacket.getOrder());
                 switch (serverPacket.getPacketType()) {
                     case 0 -> {
                         logger.debug("sending my name");
-                        ClientPacket meme =
-                                new ClientPacket(
-                                        PacketType.INITIALISE,
-                                        ((ServerPacket) o).getOrder(),
-                                        new Reply<String>("meme"));
+                        ClientPacket meme = new ClientPacket(PacketType.INITIALISE, ((ServerPacket) o).getOrder(),
+                                new Reply<String>("meme"));
                         out2.writeObject(meme);
                     }
                     case PacketType.LOAD_SCENE -> {
-                        logger.info(
-                                "Pretend im loading scene: {}", serverPacket.getOrder().getValue());
+                        logger.info("Pretend im loading scene: {}", serverPacket.getOrder().getValue());
                         if (serverPacket.getOrder().getValue().equals("gameOn")) {
                             logger.info("ENTER INPUT NOW DUMMY");
-                            Thread t =
-                                    new Thread() {
-                                        public void run() {
-                                            System.out.println("you may enter stuff now");
-                                            BufferedReader bufferedReader =
-                                                    new BufferedReader(
-                                                            new InputStreamReader(System.in));
-                                            while (true) {
-                                                try {
-                                                    String input = bufferedReader.readLine();
-                                                    ClientPacket textMessage =
-                                                            new ClientPacket(
-                                                                    PacketType.GAME_INPUT,
-                                                                    null,
-                                                                    new Reply<>(input));
-                                                    out2.writeObject(textMessage);
-                                                } catch (IOException exception) {
-                                                    exception.printStackTrace();
-                                                }
-
-                                                //
-                                                //
-                                                // ClientPacket textMessage = new
-                                                // ClientPacket(PacketType.GAME_INPUT, null, new
-                                                // Reply<>("helloo" + System.currentTimeMillis()));
-                                                //                                        try {
-                                                //
-                                                // Thread.sleep(5000);
-                                                //
-                                                // out2.writeObject(textMessage);
-                                                //
-                                                // logger.info("sent spam message");
-                                                //                                        } catch
-                                                // (IOException | InterruptedException exception) {
-                                                //
-                                                // exception.printStackTrace();
-                                                //                                        }
-                                            }
+                            Thread t = new Thread() {
+                                public void run() {
+                                    System.out.println("you may enter stuff now");
+                                    BufferedReader bufferedReader = new BufferedReader(
+                                            new InputStreamReader(System.in));
+                                    while (true) {
+                                        try {
+                                            String input = bufferedReader.readLine();
+                                            ClientPacket textMessage = new ClientPacket(PacketType.GAME_INPUT, null,
+                                                    new Reply<>(input));
+                                            out2.writeObject(textMessage);
+                                        } catch (IOException exception) {
+                                            exception.printStackTrace();
                                         }
-                                    };
+
+                                        //
+                                        //
+                                        // ClientPacket textMessage = new
+                                        // ClientPacket(PacketType.GAME_INPUT, null, new
+                                        // Reply<>("helloo" + System.currentTimeMillis()));
+                                        // try {
+                                        //
+                                        // Thread.sleep(5000);
+                                        //
+                                        // out2.writeObject(textMessage);
+                                        //
+                                        // logger.info("sent spam message");
+                                        // } catch
+                                        // (IOException | InterruptedException exception) {
+                                        //
+                                        // exception.printStackTrace();
+                                        // }
+                                    }
+                                }
+                            };
                             t.start();
                         }
                     }
                     case 4 -> {
                         logger.info("Loading image hash: {}", serverPacket.getOrder().getValue());
-                        ClientPacket imageLoaded =
-                                new ClientPacket(
-                                        PacketType.LOAD_IMG,
-                                        serverPacket.getOrder(),
-                                        new Reply<Boolean>(true));
+                        ClientPacket imageLoaded = new ClientPacket(PacketType.LOAD_IMG, serverPacket.getOrder(),
+                                new Reply<Boolean>(true));
                         out2.writeObject(imageLoaded);
-                        logger.info(
-                                "replied to load image: {},{},{}",
-                                imageLoaded.getPacketType(),
-                                imageLoaded.getReplyingTo(),
-                                imageLoaded.getReply());
+                        logger.info("replied to load image: {},{},{}", imageLoaded.getPacketType(),
+                                imageLoaded.getReplyingTo(), imageLoaded.getReply());
                     }
                     case 5 -> {
                         System.out.println("send input to server: ");
 
-                        ClientPacket gameInput =
-                                new ClientPacket(
-                                        PacketType.GAME_INPUT,
-                                        serverPacket.getOrder(),
-                                        new Reply<>(kb.nextLine()));
+                        ClientPacket gameInput = new ClientPacket(PacketType.GAME_INPUT, serverPacket.getOrder(),
+                                new Reply<>(kb.nextLine()));
                         out2.writeObject(gameInput);
                     }
                     case PacketType.CHAT_MESSAGE -> {
